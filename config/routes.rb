bdd020f392858f7943816c5f397a66f1dd64994a
Chris Hintz <chrishintz42@gmail.com>
2015-12-23 16:48:51 -0800

    added Users and Microposts controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,30 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+
+  # get     "/users",       to: "users#index"
+  # get     "users/:id",    to: "users#show",     as: :user
+  # get     "users/new",    to: "users#new",      as: :new_user
+  # post    "users",        to: "users#create"
+  # get     "users/1/edit", to: "users#edit",     as: :edit_user
+  # patch   "users/1",      to: "users#update"
+  # delete  "users/1",      to: "users#destroy"
+  #
+  # get     "/microposts",        to: "microposts#index"
+  # get     "microposts/1",       to: "microposts#show",      as: :micropost
+  # get     "/microposts/new",    to: "microposts#new",       as: :new_micropost
+  # post    "/microposts",        to: "microposts#create"
+  # get     "microposts/1/edit",  to: "microposts#edit",      as: :edit_micropost
+  # patch   "microposts/1",       to: "microposts#update"
+  # delete  "microposts/1",       to: "microposts#destroy"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
